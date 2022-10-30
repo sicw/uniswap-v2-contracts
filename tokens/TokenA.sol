02173aaf5cc03d154ba6a327728c07df9056cfbd
sichengwen <sichengwen@kuaishou.com>
2022-10-30 12:13:27 +0800

    first commit

---
+++ tokens/TokenA.sol
@@ -0,0 +1,11 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20TokenA is ERC20 {
+    constructor(uint256 initialSupply) ERC20("TokenA", "TKA") {
+        _mint(msg.sender, initialSupply);
+    }
+}
